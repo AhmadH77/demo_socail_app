a891cec4d2cf1818503db5cb467d15ed35e2da73
Aristocrat <crataristo4@gmail.com>
2021-09-24 15:21:10 +0000

    added onboarding_screen.dart

---
+++ lib/ui/onboarding/onboarding_screen.dart
@@ -0,0 +1,131 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:social_app_demo/constants/dimens.dart';
+import 'package:social_app_demo/constants/strings.dart';
+import 'package:social_app_demo/widget/auth_button.dart';
+
+class OnboardingPage extends StatefulWidget {
+  static const routeName = '/onboardingPage';
+
+  const OnboardingPage({Key? key}) : super(key: key);
+
+  @override
+  _OnboardingPageState createState() => _OnboardingPageState();
+}
+
+class _OnboardingPageState extends State<OnboardingPage> {
+  int currentIndex = 0;
+  PageController? _pageController;
+
+  @override
+  void initState() {
+    _pageController = PageController(initialPage: 0);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+        SystemUiOverlayStyle(statusBarColor: Colors.transparent));
+    return Scaffold(
+      backgroundColor: Colors.teal, //todo remove
+      body: Stack(
+        alignment: Alignment.bottomCenter,
+        children: [
+          PageView(
+            controller: _pageController,
+            onPageChanged: (index) {
+              setState(() {
+                currentIndex = index;
+              });
+            },
+            children: [
+              //image 1
+              backgroundImages('assets/svg/back.svg'),
+              //image 2
+              backgroundImages('assets/svg/back.svg'),
+              //image 3
+              backgroundImages('assets/svg/back.svg'),
+              //image 4
+              backgroundImages('assets/svg/back.svg'),
+              //image 5
+              backgroundImages('assets/svg/back.svg'),
+            ],
+          ),
+          Align(
+            alignment: Alignment.center,
+            child: Container(
+              margin: EdgeInsets.only(top: hundredDp),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: indicatorCount(),
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.center,
+            child: Container(
+              margin: EdgeInsets.only(top: twoHundredDp),
+              child: AuthButton(
+                buttonName: signup,
+                onButtonTapped: () {
+                  //todo
+                },
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.center,
+            child: Container(
+              margin: EdgeInsets.only(top: threeTwentyDp),
+              child: AuthButton(
+                buttonName: login,
+                onButtonTapped: () {
+                  //todo
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  //animated container to show indicator
+  Widget indicator(bool isActive) {
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 300),
+      height: eightDp,
+      width: eightDp,
+      margin: EdgeInsets.only(right: sixDp),
+      decoration: BoxDecoration(
+          color: isActive ? Colors.white : Colors.white.withOpacity(0.5),
+          borderRadius: BorderRadius.circular(fourDp)),
+    );
+  }
+
+  //list of indicators to show when user swipes
+  List<Widget> indicatorCount() {
+    List<Widget> indicators = [];
+    for (int i = 0; i < 5; i++) {
+      if (currentIndex == i) {
+        indicators.add(indicator(true));
+      } else {
+        indicators.add(indicator(false));
+      }
+    }
+
+    return indicators;
+  }
+
+  //displays bg images
+  Widget backgroundImages(String image) {
+    return SvgPicture.asset(
+      image,
+      height: MediaQuery.of(context).size.height,
+      width: MediaQuery.of(context).size.width,
+    );
+  }
+}
