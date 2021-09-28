52b7d9686a0d947bed18bcf2cf90e89c8774512e
Aristocrat <crataristo4@gmail.com>
2021-09-28 16:45:10 +0000

    added email_input_widget.dart

---
+++ lib/widget/email_input_widget.dart
@@ -0,0 +1,40 @@
+import 'package:email_validator/email_validator.dart';
+import 'package:flutter/material.dart';
+import 'package:social_app_demo/constants/dimens.dart';
+import 'package:social_app_demo/constants/strings.dart';
+
+class EmailInput extends StatefulWidget {
+  final TextEditingController emailController;
+
+  const EmailInput({Key? key, required this.emailController})
+      : super(
+          key: key,
+        );
+
+  @override
+  _EmailInputState createState() => _EmailInputState();
+}
+
+class _EmailInputState extends State<EmailInput> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: sixteenDp),
+      child: TextFormField(
+          keyboardType: TextInputType.emailAddress,
+          controller: widget.emailController,
+          validator: (value) =>
+              EmailValidator.validate(value!) == true ? null : invalidEmail,
+          decoration: InputDecoration(
+            hintText: email,
+            hintStyle: TextStyle(inherit: true, color: Colors.white),
+            focusedBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.white, width: 2)),
+            enabledBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.white, width: 2)),
+            errorBorder: InputBorder.none,
+            disabledBorder: InputBorder.none,
+          )),
+    );
+  }
+}
