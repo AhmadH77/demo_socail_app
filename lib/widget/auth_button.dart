0a3aa176b392666581eec1bd46aba76306a51c9d
Aristocrat <crataristo4@gmail.com>
2021-09-28 09:38:25 +0000

    added auth button

--- lib/widget/auth_button.dart
+++ lib/widget/auth_button.dart
@@ -4,19 +4,29 @@ import 'package:social_app_demo/constants/dimens.dart';
 
 class AuthButton extends StatelessWidget {
   final buttonName;
+  final bool isAuth;
   final Function() onButtonTapped;
 
   const AuthButton(
-      {Key? key, required this.buttonName, required this.onButtonTapped})
+      {Key? key,
+      required this.buttonName,
+      required this.onButtonTapped,
+      required this.isAuth})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.symmetric(horizontal: eightyDp, vertical: twentyDp),
+      margin: EdgeInsets.only(
+          right: isAuth ? tenDp : hundredDp,
+          top: isAuth ? 5 : twentyDp,
+          left: isAuth ? 0 : hundredDp),
       height: fortyDp,
       child: MaterialButton(
         shape: RoundedRectangleBorder(
+            side: BorderSide(
+                color: isAuth ? Colors.white : Colors.white,
+                width: isAuth ? 2 : 0),
             borderRadius: BorderRadius.circular(twentyDp)),
         minWidth: MediaQuery.of(context).size.width,
         height: fiftyDp,
@@ -24,11 +34,11 @@ class AuthButton extends StatelessWidget {
         child: Text(
           buttonName,
           style: TextStyle(
-              color: Colors.black,
+              color: isAuth ? Colors.white : Colors.black,
               fontSize: sixteenDp,
               fontWeight: FontWeight.bold),
         ),
-        color: Colors.white,
+        color: isAuth ? Colors.transparent : Colors.white,
       ),
     );
   }
