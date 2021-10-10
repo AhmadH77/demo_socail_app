d937bb1186346a831575d72c9a80a8dc0941eaeb
Aristocrat <crataristo4@gmail.com>
2021-10-10 18:23:35 +0000

    modified main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,49 @@
 //@dart=2.9
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
+import 'package:social_app_demo/provider/auth_provider.dart';
 import 'package:social_app_demo/provider/currentPage.dart';
 import 'package:social_app_demo/route_generator.dart';
 import 'package:social_app_demo/ui/splash_screen/splash_screen_page.dart';
 
 import 'constants/theme_color.dart';
 
-void main() {
-  runApp(MultiProvider(providers: [
-    ChangeNotifierProvider(create: (_) => CurrentPage()),
-  ], child: MyApp()));
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then((value) => runApp(VasukamApp()));
 }
 
-class MyApp extends StatelessWidget {
+class VasukamApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Social App Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        fontFamily: 'regular',
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => CurrentPage()),
+        //authentication
+        ChangeNotifierProvider.value(value: AuthProvider()),
+
+        StreamProvider<User>.value(
+          value: FirebaseAuth.instance.authStateChanges(),
+          initialData: FirebaseAuth.instance.currentUser,
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Social App Demo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          fontFamily: 'regular',
 //         primarySwatch: createMaterialColor(Constants.orangeLight),
-        primarySwatch: createMaterialColor(ThemeColors.middleCURVE),
+          primarySwatch: createMaterialColor(ThemeColors.middleCURVE),
+        ),
+        //home: MainScreen(),
+        initialRoute: SplashScreenPage.routeName,
+        onGenerateRoute: RouteGenerator.generateRoute,
       ),
-      //home: MainScreen(),
-      initialRoute: SplashScreenPage.routeName,
-      onGenerateRoute: RouteGenerator.generateRoute,
     );
   }
 
